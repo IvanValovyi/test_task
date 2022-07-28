df40f36d76078186b7e86e113b4ca4dfb58e7145
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-07-28 21:32:38 +0300

    28.07.22 Changes (Optimization)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-const gulp = require('gulp');
-const svgSprite = require('gulp-svg-sprite');
+const gulp = require('gulp')
+const svgSprite = require('gulp-svg-sprite')
+const html_min = require('gulp-htmlmin')
+const css_min = require('gulp-cssmin')
+const js_min = require('gulp-jsmin')
+const img_min = require('gulp-imagemin')
+const webp = require('gulp-webp')
 
 function svgsprite(){
     let config = {
@@ -13,7 +18,36 @@ function svgsprite(){
 
     return gulp.src("./images/svg/*.svg")      
         .pipe(svgSprite(config)).on('error', function(error){ console.log(error); })
-        .pipe(gulp.dest("./images"));
+        .pipe(gulp.dest("./"))
+        .pipe(gulp.dest("./build"))
 }
 
-exports.svgsprite = svgsprite;
+function html () {
+    return gulp.src('./index.html')
+        .pipe(html_min({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./build'));
+}
+
+function css () {
+    return gulp.src('./css/main.css')
+        .pipe(css_min())
+        .pipe(gulp.dest('./build/css'));
+}
+
+function js () {
+    return gulp.src('./js/*.js')
+        .pipe(js_min())
+        .pipe(gulp.dest('./build/js'));
+}
+
+function image () {
+    return gulp.src('./images/ordinary/**')
+        .pipe(img_min())
+        .pipe(webp())
+        .pipe(gulp.dest('./images/compressed'))
+        .pipe(gulp.dest('./build/images/compressed'))
+}
+
+exports.svgsprite = svgsprite
+exports.compress_images = image
+exports.build = gulp.series(html, css, js, svgsprite, image)
--- js/data.js
+++ js/data.js
@@ -1,70 +1,70 @@
 let all_work_showcase = [
     {
         name:'Project 1',
-        img_src:'./images/work_showcase/1.png'
+        img_src:'./images/compressed/work_showcase/1.webp'
     },{
         name:'Project 2',
-        img_src:'./images/work_showcase/2.png'
+        img_src:'./images/compressed/work_showcase/2.webp'
     },{
         name:'Project 3',
-        img_src:'./images/work_showcase/3.png'
+        img_src:'./images/compressed/work_showcase/3.webp'
     },{
         name:'Project 4',
-        img_src:'./images/work_showcase/4.png'
+        img_src:'./images/compressed/work_showcase/4.webp'
     },{
         name:'Project 5',
-        img_src:'./images/work_showcase/5.png'
+        img_src:'./images/compressed/work_showcase/5.webp'
     },{
         name:'Project 6',
-        img_src:'./images/work_showcase/6.png'
+        img_src:'./images/compressed/work_showcase/6.webp'
     },
 ]
 
 let all_social_media = [
     {
         name:'Facebook',
-        img_src:'./images/sprite.svg#facebook',
+        img_src:'./sprite.svg#facebook',
         link:'#'
     },{
         name:'Twitter',
-        img_src:'./images/sprite.svg#twi',
+        img_src:'./sprite.svg#twi',
         link:'#'
     },{
         name:'Linkedin',
-        img_src:'./images/sprite.svg#linkedin',
+        img_src:'./sprite.svg#linkedin',
         link:'#'
     },{
         name:'Behance',
-        img_src:'./images/sprite.svg#behance',
+        img_src:'./sprite.svg#behance',
         link:'#'
     },{
         name:'Dribbble',
-        img_src:'./images/sprite.svg#dribbble',
+        img_src:'./sprite.svg#dribbble',
         link:'#'
     },{
         name:'Instagram',
-        img_src:'./images/sprite.svg#insta',
+        img_src:'./sprite.svg#insta',
         link:'#'
     },
 ]
 
 let all_dribble_shots = [
     {
-        img_src:'./images/dribble_shots/1.png'
+        img_src:'./images/compressed/dribble_shots/1.webp'
     },{
-        img_src:'./images/dribble_shots/2.png'
+        img_src:'./images/compressed/dribble_shots/2.webp'
     },{
-        img_src:'./images/dribble_shots/3.png'
+        img_src:'./images/compressed/dribble_shots/3.webp'
     },{
-        img_src:'./images/dribble_shots/4.png'
+        img_src:'./images/compressed/dribble_shots/4.webp'
     },{
-        img_src:'./images/dribble_shots/5.png'
+        img_src:'./images/compressed/dribble_shots/5.webp'
     },{
-        img_src:'./images/dribble_shots/6.png'
+        img_src:'./images/compressed/dribble_shots/6.webp'
     },{
-        img_src:'./images/dribble_shots/7.png'
+        img_src:'./images/compressed/dribble_shots/7.webp'
     },{
-        img_src:'./images/dribble_shots/8.png'
+        img_src:'./images/compressed/dribble_shots/8.webp'
     },
 ]
 
